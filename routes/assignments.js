dac751761d360d0a5316b7d95b0499d79a98da99
NerdyItalian <atava8384@gmail.com>
2015-05-15 08:48:28 -0500

    working ascending and descending function, added search by name function

--- routes/assignments.js
+++ routes/assignments.js
@@ -4,49 +4,50 @@ var assignments = require('../models/assignment');
 
 /* GET /assignments listing. */
 router.get('/', function(req, res, next) {
-  assignments.find(function (err, assignments) {
-    if (err) return next(err);
-    res.json(assignments);
-  });
+    assignments.find(function (err, assignments) {
+        if (err) return next(err);
+        res.json(assignments);
+    });
 });
 
 /* POST /assignments */
 router.post('/', function(req, res, next) {
-  assignments.create(req.body, function (err, assignment) {
-    if (err) return next(err);
-    res.json(assignment);
-  });
-});
-
-/* GET /assignments/search/note */
-router.get('/search/:name', function(req, res, next) {
-  assignments.find({name: new RegExp(req.params.name, 'i')}, 'name',
-      {
-          sort: {
-              name: 1
-          }
-      },
-
-      function (err, assignment) {
+    assignments.create(req.body, function (err, assignment) {
         if (err) return next(err);
         res.json(assignment);
-      });
+    });
+});
+
+/* GET /assignments/id */
+router.get('/search/:name', function(req, res, next) {
+    console.log("Request body: ",req);
+    assignments.find({name: new RegExp(req.params.name, 'i')},
+        null,
+        {
+            sort: {
+                name: req.query.sortOrder
+            }
+        },
+        function (err, assignment) {
+            if (err) return next(err);
+            res.json(assignment);
+        });
 });
 
 /* PUT /assignments/:id */
 router.put('/:id', function(req, res, next) {
-  assignments.findByIdAndUpdate(req.params.id, req.body, function (err, assignment) {
-    if (err) return next(err);
-    res.json(assignment);
-  });
+    assignments.findByIdAndUpdate(req.params.id, req.body, function (err, assignment) {
+        if (err) return next(err);
+        res.json(assignment);
+    });
 });
 
 /* DELETE /assignments/:id */
 router.delete('/:id', function(req, res, next) {
-  assignments.findByIdAndRemove(req.params.id, req.body, function (err, assignment) {
-    if (err) return next(err);
-    res.json(assignment);
-  });
+    assignments.findByIdAndRemove(req.params.id, req.body, function (err, assignment) {
+        if (err) return next(err);
+        res.json(assignment);
+    });
 });
 
 console.log('assignments route loaded');
